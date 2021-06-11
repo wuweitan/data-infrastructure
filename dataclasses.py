968e1a3d95af217f49ac4f7d8e233047806784dc
RujYin <rujieyin@usc.edu>
2021-06-11 11:44:17 -0500

    June 6th update

--- dataclasses.py
+++ dataclasses.py
@@ -1,4 +1,6 @@
 import atomium
+import uniChainMapping
+import 
 
 class ProtPairs:
 	def __init__(self, Input = None, re_path = 'data'): # for every row, [uniprot1, uniprot2, pdbid]
@@ -14,10 +16,16 @@ class ProtPairs:
 
 	def loadData(self):
 		pass
-
+# May 28
 # TODO1: which domains are interacting with each other (write the class template)
 # TODO2: The atom availability statistics of the pdb
 # TODO3: Complete the complex class
+
+# June 4
+# TODO1: write the helper functions to find the matching relationship between uniprot ids and chain ids (using APIs)
+# TODO2: find the mapping from chain ids to the seq
+
+
 
 class AtomStatistics(ProtPairs):
 	def __init__(self, Input = None, re_path = 'data'):
@@ -47,8 +55,39 @@ class Seq(ProtPairs):
 	def __init__(self, Input = None, re_path = 'data'):
 		super().__init__(Input, re_path)
 
-	def getSeqInfoForEachUniprot(self): #(return [[chain_id4Uni1, sequences4CorChains], [chain_id4Uni2, sequences4CorChains]])
-		pass
+	def getSeqInfoForEachUniprot(self): 
+		"""
+		Output dictionary (chainid : sequences)
+	
+		"""
+		dict_chainSeq = {}
+		uniprot_list = [self.uni1, self.uni2]
+		mapp = uniChainMapping(self.PDBid, uniprot_list)
+
+		pdb_id = self.PDBid
+
+
+		pdbl = PDBList()
+
+		pdbl.retrieve_pdb_file(pdb_id)
+
+		parser = MMCIFParser()
+
+		path_to_cif = pdb_id[1]+pdb_id[2]+'/'+pdb_id+'.cif'
+
+		structure = parser.get_structure(pdb_id, path_to_cif)
+
+		if mapp == None:
+			return None
+		else:
+			for chain_list in mapp:
+				for chain in chain_list:
+					dict_chainSeq[chain] = structure[0][chain_id].get_residues()
+
+		return dict_chainSeq
+
+
+
 
 class Domain(ProtPairs):
 	def __init__(self, Input = None, re_path = 'data'):
