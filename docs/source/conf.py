f67dccaeac3c7c3636e0bc5fd9c8b502126f5214
wuweitan <59939651+wuweitan@users.noreply.github.com>
2022-08-20 21:52:50 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Data infrastructure'
+copyright = '2022, Shen group'
+author = 'Yuanfei'
 
 release = '0.1'
 version = '0.1.0'
